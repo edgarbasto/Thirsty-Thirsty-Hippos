22ea58b21bbc3f72fa83aa3dbc0356c34504d5e1
Edgar Basto <edgar.pt@gmail.com>
2018-06-06 17:28:13 +0100

    hipo moving and beer coming up

--- Thirsty Thirsty Hippos/MainWindow.xaml.cs
+++ Thirsty Thirsty Hippos/MainWindow.xaml.cs
@@ -46,22 +46,35 @@ namespace Thirsty_Thirsty_Hippos
 
                     if (isLeftPressed)
                     {
-                        this.txtkey.Text = "LEFT";
-                        this.image.Margin = new Thickness(image.Margin.Left + 5, image.Margin.Top, image.Margin.Right - 5, image.Margin.Bottom);
+                        txtkey.Text = "INVOKE LEFT";
+                        //this.image.Margin = new Thickness(image.Margin.Left + 5, image.Margin.Top, image.Margin.Right - 5, image.Margin.Bottom);
+
+                        var img = new Image();
+                        img.Width = 100;
+                        img.Height = 100;
+                        BitmapImage bi3 = new BitmapImage();
+                        bi3.BeginInit();
+                        bi3.UriSource = new Uri("images/sagres.png", UriKind.Relative);
+                        bi3.EndInit();
+                        img.Source = bi3;
+                        //img.Source = "images/sagres.png".ToString();
+                        theGrid.Children.Add(img);
+
+
                     }
                 }
             );
         }
 
 
-        private void OnKeyDownHandler(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Return)
-            {
-                textBlock1.Text = "You Entered: " + textBox1.Text;
-                //textBox1.Text = "You Entered: " + textBox1.Text;
-            }
-        }
+        //private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Return)
+        //    {
+        //        textBlock1.Text = "You Entered: " + textBox1.Text;
+        //        //textBox1.Text = "You Entered: " + textBox1.Text;
+        //    }
+        //}
 
         private void image_KeyDown(object sender, KeyEventArgs e)
         {
@@ -71,38 +84,59 @@ namespace Thirsty_Thirsty_Hippos
                 //Keyboard.IsKeyDown(Key.A)
                 isLeftPressed = true;
                 //this.txtkey.Text = "LEFT LEFT";
-                this.txtkey.Text = "POIS POIS";
+                txtkey.Text = "LEFT:" + isLeftPressed.ToString();
+                txtcoord.Text = image.Margin.Left.ToString() +", "+ image.Margin.Top.ToString() + ", " + image.Margin.Right.ToString() + ", " + image.Margin.Bottom.ToString();
+                if (image.Margin.Left > 1) {
+                    image.Margin = new Thickness(image.Margin.Left - 5, image.Margin.Top, image.Margin.Right + 5, image.Margin.Bottom);
+                }
+                
+
+
             }
 
             if (e.Key == Key.Right)
             {
                 isRightPressed = true;
+                txtkey.Text = "RIGHT:" + isRightPressed.ToString();
+                txtcoord.Text = image.Margin.Left.ToString() + ", " + image.Margin.Top.ToString() + ", " + image.Margin.Right.ToString() + ", " + image.Margin.Bottom.ToString();
+                if (image.Margin.Right > -325)
+                {
+                    image.Margin = new Thickness(image.Margin.Left + 5, image.Margin.Top, image.Margin.Right - 5, image.Margin.Bottom);
+                }
+
             }
 
         }
 
-        private void txtbox_KeyDown(object sender, KeyEventArgs e)
-        {
-
-            if (e.Key == Key.L) {
-                
-                this.txtkey.Text = "POIS POIS";
-            }
-        }
 
         private void image_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Left)
             {
                 isLeftPressed = false;
+                txtkey.Text = "LEFT:" + isLeftPressed.ToString();
             }
 
             if (e.Key == Key.Right)
             {
                 isRightPressed = false;
+                txtkey.Text = "RIGHT:" + isRightPressed.ToString();
             }
 
 
         }
-    }
-}
+
+        
+        //Resolução do FOCUS na GRID.
+        private void theGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            theGrid.Focus();
+        }
+
+        
+
+
+
+
+    } //mainwidow
+} //namespace
