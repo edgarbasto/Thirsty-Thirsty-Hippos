3a56481e3c7984ccc1f625a362ab4ca6c4d701a0
Edgar Basto <edgar.pt@gmail.com>
2018-06-08 19:53:29 +0100

    update

--- Thirsty Thirsty Hippos/MainWindow.xaml.cs
+++ Thirsty Thirsty Hippos/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.Timers;
-
+using System.Windows.Media.Animation;
 
 namespace Thirsty_Thirsty_Hippos
 {
@@ -46,19 +46,19 @@ namespace Thirsty_Thirsty_Hippos
 
                     if (isLeftPressed)
                     {
-                        txtkey.Text = "INVOKE LEFT";
-                        //this.image.Margin = new Thickness(image.Margin.Left + 5, image.Margin.Top, image.Margin.Right - 5, image.Margin.Bottom);
+                        //txtkey.Text = "INVOKE LEFT";
+                        ////this.image.Margin = new Thickness(image.Margin.Left + 5, image.Margin.Top, image.Margin.Right - 5, image.Margin.Bottom);
 
-                        var img = new Image();
-                        img.Width = 100;
-                        img.Height = 100;
-                        BitmapImage bi3 = new BitmapImage();
-                        bi3.BeginInit();
-                        bi3.UriSource = new Uri("images/sagres.png", UriKind.Relative);
-                        bi3.EndInit();
-                        img.Source = bi3;
-                        //img.Source = "images/sagres.png".ToString();
-                        theGrid.Children.Add(img);
+                        //var img = new Image();
+                        //img.Width = 100;
+                        //img.Height = 100;
+                        //BitmapImage bi3 = new BitmapImage();
+                        //bi3.BeginInit();
+                        //bi3.UriSource = new Uri("images/sagres.png", UriKind.Relative);
+                        //bi3.EndInit();
+                        //img.Source = bi3;
+                        ////img.Source = "images/sagres.png".ToString();
+                        //theGrid.Children.Add(img);
 
 
                     }
@@ -131,9 +131,98 @@ namespace Thirsty_Thirsty_Hippos
         private void theGrid_Loaded(object sender, RoutedEventArgs e)
         {
             theGrid.Focus();
+
+
+
+
+            //Animação cerveja
+            //ThicknessAnimation thicknessanimation = new ThicknessAnimation();
+
+
+            //thicknessanimation.From = new Thickness(0, 10, 683, 461);
+            //thicknessanimation.To = new Thickness(300, 300, this.Margin.Right, this.Margin.Bottom);
+            //thicknessanimation.Duration = TimeSpan.FromSeconds(1);
+
+          
+
+            //sagres1.BeginAnimation(MarginProperty, thicknessanimation);
+
+            //Completed não funciona ):
+
+            //HandoffBehavior handoffBehavior = new HandoffBehavior();
+            //sagres1.ManipulationCompleted += Sagres1_ManipulationCompleted;
+
+            CervejaAnimation teste = new CervejaAnimation();
+            teste.ThicknessAnimationExample();
+            
         }
 
         
+
+        private void Sagres1_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            txtmov.Text = "ANIMATION COMPLETED";
+        }
+
+
+        public class CervejaAnimation : Page
+        {
+            public void ThicknessAnimationExample()
+            {
+
+                // TESTE COM STORYBOARD ----------------------------------------------------------
+
+                //Namescope para esta página
+                NameScope.SetNameScope(this, new NameScope());
+
+                //Criar a imagem
+                Image sagrespt = new Image();
+
+                sagrespt.Width = 100;
+                sagrespt.Height = 100;
+                BitmapImage bi3 = new BitmapImage();
+                bi3.BeginInit();
+                bi3.UriSource = new Uri("images/sagres.png", UriKind.Relative);
+                bi3.EndInit();
+                sagrespt.Source = bi3;
+
+                this.RegisterName("imgAnimated", sagrespt);
+
+                //Definição da animação 
+                ThicknessAnimation thicknessanimation = new ThicknessAnimation();
+
+
+                thicknessanimation.From = new Thickness(0, 10, 683, 461);
+                thicknessanimation.To = new Thickness(300, 300, this.Margin.Right, this.Margin.Bottom);
+                thicknessanimation.Duration = TimeSpan.FromSeconds(1);
+
+
+                //Atribuir a animação
+                Storyboard.SetTargetName(thicknessanimation, "imgAnimated");
+                Storyboard.SetTargetProperty(thicknessanimation, new PropertyPath(MarginProperty));
+
+                //Criação da storyboard
+                Storyboard moveCerveja = new Storyboard();
+                moveCerveja.Children.Add(thicknessanimation);
+
+                sagrespt.Loaded += delegate (object sender, RoutedEventArgs e)
+                {
+                    moveCerveja.Begin(this);
+                };
+
+                StackPanel myStackPanel = new StackPanel();
+                myStackPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                myStackPanel.Children.Add(sagrespt);
+
+                Content = myStackPanel;
+
+            }
+
+
+        }
+
+
+
 
 
 
